90458731ecb90e34606069fe60ee85ec3522522d
jorneylopez <jorney95@live.com>
2022-10-24 22:59:16 -0600

    Herencia multiple completed

---
+++ POO/L4/test_figura_geometrica.py
@@ -0,0 +1,17 @@
+from Cuadrado import Cuadrado
+from Rectangulo import Rentangulo
+
+print('Cuadrado'.center(50,'-'))
+cuadrado1 = Cuadrado(5, 'rojo')
+# print(cuadrado1.ancho)
+# print(cuadrado1.alto)
+# print(cuadrado1.color)
+print(cuadrado1.calcularArea())
+print(cuadrado1)
+
+print('Rectangulo'.center(50,'-'))
+rentangulo1 = Rentangulo(4, 6, 'verde')
+print(rentangulo1.ancho)
+print(rentangulo1.alto)
+print(rentangulo1.CalcularRectangulo())
+print(rentangulo1)
